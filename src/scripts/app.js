e788c2fcc36dd0b46d9ce2cd396b10b493045046
JosephPoncini <jxponcini@gmail.com>
2024-01-30 22:52:25 -0800

    some button functions added

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,11 +1,49 @@
 //Initialize Things from documnet
 
-    //Initialize Btns
+    //Initialize Btns/Forms
+    let search = document.getElementById("search");
+    let randomBtn = document.getElementById("randomBtn");
+    let searchBtn = document.getElementById("searchBtn");
+    let favoritesBtn = document.getElementById("favoritesBtn");
+    let shinyBtn = document.getElementById("shinyBtn");
+    let heartBtn = document.getElementById("heartBtn");
 
     //Initialize Elements
+    let pokemonImage = document.getElementById("pokemonImage");
+    let indexNumber = document.getElementById("indexNumber");
+    let pokemonTitle = document.getElementById("pokemonTitle");
+    let descriptionTxt = document.getElementById("descriptionTxt");
+    let evolutionContainer = document.getElementById("evolutionContainer");
+    let genTxt = document.getElementById("genTxt");
+    let typeContainer = document.getElementById("typeContainer");
+    let locationTxt = document.getElementById("locationTxt");
+    let abilitiesTxt = document.getElementById("abilitiesTxt");
+    let movesTxt = document.getElementById("movesTxt");
 
 //Initialize variables
 
-//Btn Functions
+let isShiny = false;
+let isFavorited = false;
 
+//Btn Functions
+shinyBtn.addEventListener("click", () =>{
+
+    if(!isShiny){
+        isShiny = true;
+        shinyBtn.setAttribute("src","../assets/SparkleShiny.png");
+    }else{
+        isShiny = false;
+        shinyBtn.setAttribute("src","../assets/Sparkle.png");
+    }
+});
+
+heartBtn.addEventListener("click", () => {
+    if(!isFavorited){
+        isFavorited = true;
+        heartBtn.setAttribute("src","../assets/HeartFilled.png");
+    }else{
+        isFavorited = false;
+        heartBtn.setAttribute("src","../assets/Heart.png");
+    }
+})
 //fetch functions
